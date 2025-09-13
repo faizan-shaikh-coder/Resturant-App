a78c1d99904b53d1907823ddbfb0e4c133449431
vishal <vp8781532@gmail.com>
2025-09-13 15:13:34 +0530

    nav added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
-const Nav = () => {
+export default function Navbar() {
   return (
-    <div>
-      
-    </div>
-  )
+    <nav className="bg-white shadow-md fixed w-full z-50">
+      <div className="container mx-auto flex justify-between items-center py-4 px-6">
+        <h1 className="text-2xl font-bold text-green-600">Gadly</h1>
+        <ul className="flex space-x-6 font-medium">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/menu">Menu</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/ideas">Ideas</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
-
-export default Nav
